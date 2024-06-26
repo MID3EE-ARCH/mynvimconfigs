230bbc23591c15677179ef953181bcf3ad740e1e
MID3EE-ARCH <realphoenix484@gmail.com>
2024-06-26 21:13:11 +0530

    Updated the nvim configuration

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,15 +1,52 @@
 local config = function()
 	require("bufferline").setup({
 		options = {
+			theme = "auto",
+			globalstatus = true,
 			color_icons = true,
-			theme = "tokyonight",
 		},
-    highlights = {
-      fill={
-        ctermbg = '24283b',
-        bg = '#24283b',
-      }
-    }
+		highlights = {
+			fill = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			background = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			buffer_selected = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			buffer_visible = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			close_button = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			close_button_selected = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			close_button_visible = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			separator = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			separator_selected = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+			separator_visible = {
+				ctermbg = "24283b",
+				bg = "#24283b",
+			},
+		},
 	})
 end
 
